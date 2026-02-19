434bb7c5865ee4cfb38ab594b376443632dcb668
Shi Bai <shi.bai@monash.edu>
2026-02-19 23:25:47 +1100

    added contributed files of siibra-explorer to support Marmoset Nencki-Monash template

---
+++ siibra-explorer/src/atlasComponents/sapi/constants.ts
@@ -0,0 +1,35 @@
+export const IDS = {
+  ATLAES: {
+    HUMAN: "juelich/iav/atlas/v1.0.0/1",
+    RAT: "minds/core/parcellationatlas/v1.0.0/522b368e-49a3-49fa-88d3-0870a307974a",
+    MARMOSET: "nencki/nm/atlas/v1.0.0/marmoset",
+  },
+  TEMPLATES: {
+    BIG_BRAIN: "minds/core/referencespace/v1.0.0/a1655b99-82f1-420f-a3c2-fe80fd4c8588",
+    MNI152: "minds/core/referencespace/v1.0.0/dafcffc5-4826-4bf1-8ff6-46b8a31ff8e2",
+    COLIN27: "minds/core/referencespace/v1.0.0/7f39f7be-445b-47c0-9791-e971c0b6d992",
+    WAXHOLM: "minds/core/referencespace/v1.0.0/d5717c4a-0fa1-46e6-918c-b8003069ade8",
+    MEBRAINS: "minds/core/referencespace/v1.0.0/MEBRAINS",
+    FSAVERAGE: 'minds/core/referencespace/v1.0.0/tmp-fsaverage',
+    AMBA_CCF_V3: "minds/core/referencespace/v1.0.0/265d32a0-3d84-40a5-926f-bf89f68212b9",
+    MARMOSET_NM: "nencki/nm/referencespace/v1.0.0/MARMOSET_NM_NISSL_2020",
+  },
+  PARCELLATION: {
+    JBA29: "minds/core/parcellationatlas/v1.0.0/94c1125b-b87e-45e4-901c-00daee7f2579-290",
+    JBABB: "minds/core/parcellationatlas/v1.0.0/94c1125b-b87e-45e4-901c-00daee7f2579-bb",
+    JBA30: "minds/core/parcellationatlas/v1.0.0/94c1125b-b87e-45e4-901c-00daee7f2579-300",
+    JBA31: "minds/core/parcellationatlas/v1.0.0/94c1125b-b87e-45e4-901c-00daee7f2579-310",
+    WAXHOLMV4: "minds/core/parcellationatlas/v1.0.0/ebb923ba-b4d5-4b82-8088-fa9215c2e1fe-v4",
+    CORTICAL_LAYERS: "juelich/iav/atlas/v1.0.0/3",
+    MEBRAINS: "minds/core/parcellationatlas/v1.0.0/e3235c039c6f54c3ba151568c829f117",
+    JBABBV2: "jba-bb-v2",
+    SULCI: "atlas-humansulci",
+    VON_ECONOMO: "minds/core/parcellationatlas/v1.0.0/f2b1ac621421708c1bef422bb5058456",
+    MARMOSET_NM: "nencki/nm/parcellationatlas/v1.0.0/MARMOSET_NM",
+  }
+}
+
+export const MAPTYPE = {
+  STATISTICAL: "STATISTICAL",
+  LABELLED: "LABELLED"
+} as const
---
+++ siibra-explorer/src/util/constants.ts
@@ -0,0 +1,113 @@
+import { HttpHeaders } from "@angular/common/http"
+
+export const LOCAL_STORAGE_CONST = {
+  GPU_LIMIT: 'fzj.xg.iv.GPU_LIMIT',
+  ANIMATION: 'fzj.xg.iv.DISABLE_ANIMATION_FLAG',
+  MOBILE_UI: 'fzj.xg.iv.MOBILE_UI',
+  AGREE_COOKIE: 'fzj.xg.iv.AGREE_COOKIE',
+  AGREE_KG_TOS: 'fzj.xg.iv.AGREE_KG_TOS',
+  QUICK_TOUR_VIEWED: 'fzj.dg.iv.QUICK_TOUR_VIEWED',
+}
+
+export const COOKIE_VERSION = '0.3.0'
+export const KG_TOS_VERSION = '0.3.0'
+
+export const MIN_REQ_EXPLAINER = `
+- Siibra explorer requires **webgl2.0**, and the \`EXT_color_buffer_float\` extension enabled.
+- You can check browsers' support of webgl2.0 by visiting <https://caniuse.com/#feat=webgl2>
+- Unfortunately, Safari and iOS devices currently do not support **webgl2.0**: <https://webkit.org/status/#specification-webgl-2>
+`
+
+export const APPEND_SCRIPT_TOKEN: InjectionToken<(url: string) => Promise<HTMLScriptElement>> = new InjectionToken(`APPEND_SCRIPT_TOKEN`)
+
+export const appendScriptFactory = (document: Document, defer: boolean = false) => {
+  return (src: string) => new Promise((rs, rj) => {
+    const scriptEl = document.createElement('script')
+    if (defer) {
+      scriptEl.defer = true
+    }
+    scriptEl.src = src
+    scriptEl.onload = () => rs(scriptEl)
+    scriptEl.onerror = (e) => rj(e)
+    document.head.appendChild(scriptEl)
+  })
+}
+
+export const REMOVE_SCRIPT_TOKEN: InjectionToken<(el: HTMLScriptElement) => void> = new InjectionToken(`REMOVE_SCRIPT_TOKEN`)
+
+export const removeScriptFactory = (document: Document) => {
+  return (srcEl: HTMLScriptElement) => {
+    document.head.removeChild(srcEl)
+  }
+}
+
+const getScopedReferer = () => {
+  const url = new URL(window.location.href)
+  url.searchParams.delete('regionsSelected')
+  url.searchParams.delete('cRegionsSelected')
+  return url.toString()
+}
+
+export const getFetchOption: () => Partial<RequestInit> = () => {
+  return {
+    referrer: getScopedReferer()
+  }
+}
+
+export const getHttpHeader: () => HttpHeaders = () => {
+  const header = new HttpHeaders()
+  header.set('referrer', getScopedReferer())
+  return header
+}
+
+export const COLORMAP_IS_JET = `// iav-colormap-is-jet`
+import { InjectionToken } from "@angular/core"
+
+export const CYCLE_PANEL_MESSAGE = `[spacebar] to cycle through views`
+
+export const UNSUPPORTED_PREVIEW = [{
+  text: 'Preview of Colin 27 and JuBrain Cytoarchitectonic',
+  previewSrc: 'assets/images/1.png',
+}, {
+  text: 'Preview of Big Brain 2015 Release',
+  previewSrc: 'assets/images/2.png',
+}, {
+  text: 'Preview of Waxholm Rat V2.0',
+  previewSrc: 'assets/images/3.png',
+}]
+
+export const UNSUPPORTED_INTERVAL = 7000
+
+export const SPECIES_ENUM = {
+  HOMO_SAPIENS: "Homo sapiens",
+  MACACA_FASCICULARIS: "Macaca fascicularis",
+  RATTUS_NORVEGICUS: "Rattus norvegicus",
+  MUS_MUSCULUS: "Mus musculus",
+  CALLITHRIX_JACCHUS: "Callithrix jacchus",
+} as const
+
+/**
+ * atlas should follow the following order
+ */
+export const speciesOrder = [
+  SPECIES_ENUM.MUS_MUSCULUS,
+  SPECIES_ENUM.RATTUS_NORVEGICUS,
+  SPECIES_ENUM.MACACA_FASCICULARIS,
+  SPECIES_ENUM.HOMO_SAPIENS,
+  SPECIES_ENUM.CALLITHRIX_JACCHUS
+]
+
+export const parcBanList: string[] = [
+  "https://identifiers.org/neurovault.image:23262", // dk
+  "https://doi.org/10.1016/j.jneumeth.2020.108983/mni152", // vep
+  "minds/core/parcellationatlas/v1.0.0/887da8eb4c36d944ef626ed5293db3ef", // marsatlas
+]
+
+export const GET_ATTR_TOKEN = new InjectionToken("GET_ATTR_TOKEN")
+
+export type GetAttr = (attr: string) => string|null
+
+export const SXPLR_ANNOTATIONS_KEY = {
+  TEMPLATE_ID: "sxplrAnnotation.template.id",
+  PARCELLATION_ID: "sxplrAnnotation.parcellation.id",
+} as const
